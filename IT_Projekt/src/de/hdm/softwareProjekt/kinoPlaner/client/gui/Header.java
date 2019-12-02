4c05ee4b0f16946c8c9bdcb390a8f37ef032ad63
jakobschaal <js325@hdm-stuttgart.de>
2019-12-02 21:23:05 +0100

    Admin Navigation Form

--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/gui/Header.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/gui/Header.java
@@ -1,5 +1,7 @@
 package de.hdm.softwareProjekt.kinoPlaner.client.gui;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 
@@ -10,6 +12,28 @@ public class Header extends HorizontalPanel{
 	private Button homeButton = new Button("Home");
 	private Button einstellungenButton = new Button("Einstellungen");
 	
+	public Header() {
+		
+	}
 	
+	private class HomeButtonClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	private class einstellungenButtonClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 
 }
--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/gui/MenuForm.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/gui/MenuForm.java
@@ -1,14 +1,87 @@
 package de.hdm.softwareProjekt.kinoPlaner.client.gui;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.RootPanel;
 
-public class MenuForm extends VerticalPanel {
-	
-	private VerticalPanel menuPanel = new VerticalPanel();
-	
-	private Button kinoketteErstellen = new Button("Kinokette erstellen");
-	private Button kinoErstellen = new Button("Kino erstellen");
-	private Button SpielplanErstellen = new Button("Spielplan erstellen");
 
+
+public class MenuForm extends FlowPanel {
+	
+	private FlowPanel navelement = new FlowPanel();
+	
+	private Button kinoketteErstellenButton = new Button("Kinokette erstellen");
+	private Button kinoErstellenButton = new Button("Kino erstellen");
+	private Button spielplanErstellenButton = new Button("Spielplan erstellen");
+
+	
+	private KinoketteErstellenForm kkef;
+	private KinoErstellenForm kef;
+	private SpielplanErstellenForm spe;
+	
+	
+	public void onLoad() {
+		
+		this.addStyleName("navcontainer");
+		navelement.addStyleName("navelement");
+		
+		kinoketteErstellenButton.addStyleName("navButton");
+		kinoErstellenButton.addStyleName("navButton");
+		spielplanErstellenButton.addStyleName("navButton");
+		
+		// Zusammenbauen der Widgets
+		this.add(navelement);
+		this.add(navelement);
+		
+		navelement.add(kinoketteErstellenButton);
+		navelement.add(kinoErstellenButton);
+		navelement.add(spielplanErstellenButton);
+		
+		// Click-Handler
+		
+		kinoketteErstellenButton.addClickHandler(new KinoketteErstellenClickHandler());
+		kinoErstellenButton.addClickHandler(new KinoErstellenClickHandler());
+		spielplanErstellenButton.addClickHandler(new SpielplanErstellenClickHandler());
+	}
+	
+	
+	
+	private class KinoketteErstellenClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+			RootPanel.get("details").clear();
+			kkef = new KinoketteErstellenForm();
+//			RootPanel.get("details").add(kkef);
+		}
+		
+	}
+	
+	private class KinoErstellenClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+			RootPanel.get("details").clear();
+			kef = new KinoErstellenForm();
+		//	RootPanel.get("details").add(kef);
+			
+		}
+		
+	}
+	
+	private class SpielplanErstellenClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+			RootPanel.get("details").clear();
+			spe = new SpielplanErstellenForm();
+		//	RootPanel.get("details").add(spe);
+		}
+		
+	}
 }
