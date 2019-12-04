0fc924f600214d5ef4f50ceb351c7549b11af445
fionadosdall <56394639+fionadosdall@users.noreply.github.com>
2019-12-04 16:59:34 +0100

    Gui minimal bearbeitet

--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/editorGui/GruppenAnzeigenForm.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/editorGui/GruppenAnzeigenForm.java
@@ -1,5 +1,30 @@
-package de.hdm.softwareProjekt.kinoPlaner.client.editorGui;
+/**
+ * package de.hdm.softwareProjekt.kinoPlaner.client.editorGui;
+ * import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.TextBox;
 
-public class GruppenAnzeigenForm {
+import de.hdm.softwareProjekt.kinoPlaner.client.ClientsideSettings;
+import de.hdm.softwareProjekt.kinoPlaner.shared.KinoplanerAsync;
+import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Anwender;
 
+public class GruppenAnzeigenForm extends FlowPanel {
+	
+	private KinoplanerAsync kinoplaner = ClientsideSettings.getKinoplaner(); 												
+	private Anwender anwender = CurrentAnwender.getAnwender();
+	private Anwender newAnwender= null;
+	
+	
+	private TextBox addAnwenderTextBox = new TextBox ();
+	private Button speicherGruppenButton = new Button ("Speichern");
+	private FlexTable mitgliedFlexTable = new FlexTable();
+	
+	
+	
 }
+ */
+
+
+
--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/editorGui/UmfragenAnzeigenForm.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/editorGui/UmfragenAnzeigenForm.java
@@ -1,5 +1,7 @@
 package de.hdm.softwareProjekt.kinoPlaner.client.editorGui;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+
 public class UmfragenAnzeigenForm {
 
 }
