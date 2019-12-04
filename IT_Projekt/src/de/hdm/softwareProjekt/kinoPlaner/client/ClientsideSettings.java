b4e4a72587104051773999089d6466f74aa47ced
MoritzHampe <moritzhampe124@web.de>
2019-12-04 23:05:37 +0100

    Erweiterung ClientsideSettings + EditorEntry + web.xml

--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/ClientsideSettings.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/ClientsideSettings.java
@@ -1,11 +1,12 @@
 package de.hdm.softwareProjekt.kinoPlaner.client;
 
+//import java.util.logging.Logger;
+
 import com.google.gwt.core.client.GWT;
 
 import de.hdm.softwareProjekt.kinoPlaner.shared.CommonSettings;
 import de.hdm.softwareProjekt.kinoPlaner.shared.Kinoplaner;
 import de.hdm.softwareProjekt.kinoPlaner.shared.KinoplanerAsync;
-
 /*
  * Klasse dient zur Bereitstellung aller Eigenschaften und Dienste 
  * für die Client Seite
@@ -21,11 +22,38 @@ public class ClientsideSettings extends CommonSettings {
 
 	/*
 	 * Remote Service Proxy zur Verbindung mit dem Server-Seitgen Dienst des
-	 * Admin-Clients
+	 * Login
 	 */
 
-	// private static AdminClientAsny adminclient = null; --> Muss noch erstellt
-	// werden
+//	private static LoginServiceAsync loginService = null:
+//		
+//	//Client-Seitig
+//		
+//	private static final String LOGGER_NAME = "Kinoplaner Web Client";
+//	
+//	//Client-Seitig
+//	
+//	private static final Logger log = Logger.getLogger(LOGGER_NAME);
+//	
+//	/*
+//	 * Auslesen des zentralen eingeloggten Anwenders
+//	 */
+//	
+//	public static Logger getLogger() {
+//		return log;
+//	}
+//
+//	/*
+//	 * Die Methode ermöglicht das erstellen einer Instant der Klasse LoginService
+//	 */
+//	public static LoginServiceAsync getLoginService() {
+//		if (loginService == null) {
+//			loginService = GWT.create(LoginService.class);
+//		}
+//		return loginService;
+//	}
+	
+	
 
 	public static KinoplanerAsync getKinoplaner() {
 
--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/EditorEntry.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/EditorEntry.java
@@ -2,12 +2,12 @@ package de.hdm.softwareProjekt.kinoPlaner.client;
 
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 
 import de.hdm.softwareProjekt.kinoPlaner.client.editorGui.Footer;
 import de.hdm.softwareProjekt.kinoPlaner.client.editorGui.Header;
 import de.hdm.softwareProjekt.kinoPlaner.client.editorGui.Navigator;
+import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Anwender;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -24,6 +24,24 @@ public class EditorEntry implements EntryPoint {
 		RootPanel.get("header").add(header);
 		RootPanel.get("navigator").add(navigator);
 		RootPanel.get("footer").add(footer);
+		
+	}
+	
+	/*
+	 * Die Klasse dient jederzeit zum Aufrufen des aktuellen Anwenders
+	 */
+	
+	public static class aktuellerAnwender {
+		
+		public static Anwender anwender = null;
+		
+		public static Anwender getAnwender() {
+			return anwender;
+		}
+		
+		public static void setAnwender(Anwender anwender) {
+			aktuellerAnwender.anwender = anwender;
+		}
 		
 	}
 }
