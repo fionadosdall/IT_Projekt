7ba86455244aeccf0b4c41e420038ae0ab1cf1ea
MoritzHampe <moritzhampe124@web.de>
2019-12-04 21:49:02 +0100

    Hinzufügen einer Home Form + HomeBar

--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/editorGui/Home.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/editorGui/Home.java
@@ -3,14 +3,15 @@ package de.hdm.softwareProjekt.kinoPlaner.client.editorGui;
 
 import com.google.gwt.user.client.ui.FlowPanel;
 
-public class ErgebnisseAnzeigenForm extends FlowPanel {
+
+public class Home extends FlowPanel {
 	
-	HomeBar hb = new HomeBar();
 	
 	private FlowPanel detailsoben = new FlowPanel();
 	private FlowPanel detailsunten = new FlowPanel();
 	
-	
+	private HomeBar hb = new HomeBar();
+
 	public void onLoad() {
 
 		this.addStyleName("detailscontainer");
@@ -18,14 +19,16 @@ public class ErgebnisseAnzeigenForm extends FlowPanel {
 		detailsoben.addStyleName("detailsoben");
 		detailsunten.addStyleName("detailsunten");
 		
+		
+		// Zusammenbauen der Widgets
 		this.add(detailsoben);
 		this.add(detailsunten);
 		
+		
 		detailsoben.add(hb);
-		
-		
-		
-	
-	}
 
-}
+		
+
+	}
+}	
+	
