cc724fea39772b16c58e4ebad4c89caf8d3d0fb2
fionadosdall <56394639+fionadosdall@users.noreply.github.com>
2019-12-06 22:00:14 +0100

    Update MeineKinokettenForm.java
    Merge rettung 2.0

--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/gui/MeineKinokettenForm.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/gui/MeineKinokettenForm.java
@@ -40,7 +40,6 @@ public class MeineKinokettenForm extends FlowPanel{
 	private Grid felder = new Grid (3,2);
 	private HomeBar hb = new HomeBar ();
 
-
 	
 	public void onLoad() {
 		KinoplanerAsync kinoplaner = ClientsideSettings.getKinoplaner();
@@ -101,11 +100,8 @@ public class MeineKinokettenForm extends FlowPanel{
 	}
 	
 
-
 private class KinoketteAuswaehlenClickHandler implements DoubleClickHandler {
 	private Kinokette kinokette;
-	
-
 
 	@Override
 	public void onDoubleClick(DoubleClickEvent event) {
@@ -127,85 +123,6 @@ private class KinoketteAuswaehlenClickHandler implements DoubleClickHandler {
 
 	
 
-	public void onLoad() {
-		KinoplanerAsync kinoplaner = ClientsideSettings.getKinoplaner();
-		
-		this.addStyleName("detailscontainer");
-		
-		detailsoben.addStyleName("detailsoben");
-		detailsunten.addStyleName("detailsunten");
-		detailsboxInhalt.addStyleName("detailsboxInhalt");
-		
-		title.addStyleName("title");
-		
-		this.add(detailsoben);
-		this.add(detailsunten);
-		this.add(detailsboxInhalt);
-		
-		detailsoben.add(hb);
-		detailsoben.add(title);
-		
-		kinokettenlabel.setStyleName("detailsboxLabels");
-		kinolabel.setStyleName("detailsboxLabels");
-		
-		kinoplaner.getAllKinoketten(new SucheKinoketteCallback());
-		
-		felder.setWidget(0, 0, kinokettenlabel);
-		felder.setWidget(0, 1, kinolabel);
-		
-		if (kinoketten != null) {
-			felder.resizeRows(kinoketten.size());
-			int i=1;
-			int j=0;
-			
-			for ( Kinokette kinokette : kinoketten) {
-				Label kinokettename = new Label (kinokette.getName());
-				
-				KinoketteAuswaehlenClickHandler click = new KinoketteAuswaehlenClickHandler();
-				click.setKinokette(kinokette);
-				kinokettename.addDoubleClickHandler(click);
-				felder.setWidget(i, 0, kinokettename);
-				j++;
-				kinoplaner.getKinoById(kinokette.getKinokettenId(),new KinoketteByIdCallback());
-				felder.setWidget(i, j, new Label(kino.getName()));
-				i++;
-				j=0;
-				kino= null;
-				}
-			
-			} else {
-					felder.setWidget(1, 0, new Label("Keine Kinoketten verfügbar"));
-					Button erstellenButton = new Button ("Erstelle deine erste Kinokette");
-					erstellenButton.setStyleName("navButton");
-					erstellenButton.addDoubleClickHandler(new KinoketteErstellenClickHandler());
-					felder.setWidget(2, 0, erstellenButton);
-		}
-		
-		this.add(felder);
-		
-	}
-	
-
-private class KinoketteAuswaehlenClickHandler implements DoubleClickHandler {
-	private Kinokette kinokette;
-
-	@Override
-	public void onDoubleClick(DoubleClickEvent event) {
-		// TODO Auto-generated method stub
-		RootPanel.get("details").clear();
-		anzeigen = new MeineKinokettenForm ();
-		anzeigen.setKinokette(kinokette);
-		RootPanel.get("details").add(anzeigen);
-	}
-	
-	public void setKinokette (Kinokette kinokette) {
-		this.kinokette = kinokette;
-	}
-}
-
-
-
-
 	private class KinoketteErstellenClickHandler implements DoubleClickHandler {
 
 		@Override
@@ -216,7 +133,6 @@ private class KinoketteAuswaehlenClickHandler implements DoubleClickHandler {
 			RootPanel.get("details").add(erstellen);
 		}
 		
-
 }
 	
 	private class SucheKinoketteCallback implements AsyncCallback {
@@ -256,50 +172,4 @@ private class KinoketteAuswaehlenClickHandler implements DoubleClickHandler {
 
 
 }
-
-
-}
-	
-	private class SucheKinoketteCallback implements AsyncCallback {
-
-		@Override
-		public void onFailure(Throwable caught) {
-			Window.alert("Kinokette nicht abrufbar");
-			
-		}
-
-	
-
-		
-		public void onSuccess (ArrayList <Kinokette> result) {
-			kinoketten = result;
-			
-		}
-		
-	}
-
-	
-
-	
-	private class KinoketteByIdCallback implements AsyncCallback <Kinokette> {
-
-		@Override
-		public void onFailure(Throwable caught) {
-			Window.alert("Kinokette nicht auffindbar");
-			
-		}
-
-		@Override
-		public void onSuccess(Kinokette result) {
-			kinokette = result;
-			
-		}
-		
-	}
-	
-
-
-
-}
-
 
