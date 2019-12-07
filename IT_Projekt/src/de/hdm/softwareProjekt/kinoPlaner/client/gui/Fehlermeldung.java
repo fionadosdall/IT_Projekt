498c6801626ac0ed6e6fa38dcc3da6024f8cbcca
Jennifer Hammen <jh216@hdm-stuttgart.de>
2019-12-07 10:23:32 +0100

    Revert "Revert "Revert "Merge branch 'master' into JenniferHammen"""
    This reverts commit 9574355c498f90899ea35a02e0ebdd991894203a.

---
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/gui/Fehlermeldung.java
@@ -0,0 +1,11 @@
+package de.hdm.softwareProjekt.kinoPlaner.client.gui;
+
+public class Fehlermeldung {
+
+	
+	public static void anzeigen(String message) {
+		
+		
+		
+	}
+}
--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/gui/KinoketteErstellenForm.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/gui/KinoketteErstellenForm.java
@@ -1,26 +1,16 @@
 package de.hdm.softwareProjekt.kinoPlaner.client.gui;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.Grid;
-import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.hdm.softwareProjekt.kinoPlaner.client.ClientsideSettings;
 import de.hdm.softwareProjekt.kinoPlaner.shared.KinoplanerAsync;
-import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Kinokette;
-
 
 public class KinoketteErstellenForm extends VerticalPanel{
 	
 	private KinoplanerAsync administration = ClientsideSettings.getKinoplaner();
-	
-	private HorizontalPanel obenPanel = new HorizontalPanel();
-	private HorizontalPanel untenPanel = new HorizontalPanel();
 	
 	private Label kinoketteFormLabel = new Label("Neue Kinokette");
 	private Label nameLabel = new Label("Kinokettenname:");
@@ -31,72 +21,9 @@ public class KinoketteErstellenForm extends VerticalPanel{
 	private TextBox sitzTextBox = new TextBox();
 	private TextBox websiteTextBox = new TextBox();
 	
-	private Grid kinoketteGrid = new Grid(4, 2);
 	private Button speichernButton = new Button("Speichern");
 	
-	private KinoketteErstellenForm kinoketteErstellenForm;
 	
-	public KinoketteErstellenForm() {
-		
-		speichernButton.addClickHandler(new SpeichernClickHandler());
-		untenPanel.add(speichernButton);
-		
-		
-	}
-	
-	
-	public void onLoad() {
-		
-		kinoketteFormLabel.setStylePrimaryName("kinoketteFormLabel");
-		nameLabel.setStylePrimaryName("nameLabel");
-		sitzLabel.setStylePrimaryName("sitzLabel");;
-		websiteLabel.setStylePrimaryName("websiteLabel");
-		speichernButton.setStylePrimaryName("speichernButton");
-		
-		
-		obenPanel.add(kinoketteFormLabel);
-		
-		kinoketteGrid.setWidget(0, 0, nameLabel);
-		kinoketteGrid.setWidget(0, 1, nameTextBox);
-		kinoketteGrid.setWidget(1, 0, sitzLabel);
-		kinoketteGrid.setWidget(1, 1, sitzTextBox);
-		kinoketteGrid.setWidget(2, 0, websiteLabel);
-		kinoketteGrid.setWidget(2, 1, websiteTextBox);
-		
-	
-		untenPanel.add(speichernButton);
-	}
-		
-		
-		
-	public class SpeichernClickHandler implements ClickHandler {
-
-		@Override
-		public void onClick(ClickEvent event) {
-			// TODO Auto-generated method stub
-			
-		}		
-		
-	}
-
-		
-	/* Callback */
-				
-	private class KinoketteErstellenCallback implements AsyncCallback<Kinokette> {
-
-		@Override
-		public void onFailure(Throwable caught) {
-			// TODO Auto-generated method stub
-			Systemmeldung.anzeigen("Eine neue Kinokette konnte leider nicht erstellt werden");
-		}
-
-		@Override
-		public void onSuccess(Kinokette result) {
-			// TODO Auto-generated method stub
-			Systemmeldung.anzeigen("Kinokette wurde angelegt");
-		}
-		
-	}
 	
 
 }
