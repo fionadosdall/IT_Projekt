6a916472c2e3f3e4c0a1525c1486a269d93d00ee
fionadosdall <56394639+fionadosdall@users.noreply.github.com>
2019-12-07 14:36:38 +0100

    MeineSpielplaeneForm bearbeitet

--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/gui/MeineSpielplaeneForm.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/gui/MeineSpielplaeneForm.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 
 import com.google.gwt.event.dom.client.DoubleClickEvent;
 import com.google.gwt.event.dom.client.DoubleClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -14,93 +17,147 @@ import de.hdm.softwareProjekt.kinoPlaner.client.ClientsideSettings;
 import de.hdm.softwareProjekt.kinoPlaner.client.editorGui.GruppeAnzeigenForm;
 import de.hdm.softwareProjekt.kinoPlaner.client.editorGui.HomeBar;
 import de.hdm.softwareProjekt.kinoPlaner.shared.KinoplanerAsync;
+import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Kino;
 import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Spielplan;
 
-public class MeineSpielplaeneForm extends VerticalPanel {
+public class MeineSpielplaeneForm extends FlowPanel {
 
-	private VerticalPanel detailsoben = new VerticalPanel();
-	private VerticalPanel detailsboxInhalt = new VerticalPanel();
-	private Label spielplan = new Label("Spielpläne");
-
-	private Button bearbeitenButton = new Button("Edit");
-
-	private SpielplanBearbeitenForm bearbeiten;
-	private SpielplanAnzeigenForm anzeigen;
-	private ArrayList<Spielplan> spielplaene;
-	// Achtung Grid-Entdeckung von Jenny&Moe: Immer eine Nr. größer angeben, als man
-	// braucht...
-	private Grid felder = new Grid(2, 2);
+	private FlowPanel detailsoben = new FlowPanel();
+	private FlowPanel detailsunten = new FlowPanel();
+	private FlowPanel detailsboxInhalt = new FlowPanel();
+	
+	
+	private Label title = new Label ("Deine Spielpläne");
+	
+	private ArrayList <Spielplan> spielplaene;
+	private Kino kino;
+	private MeineSpielplaeneForm anzeigen;
+	private SpielplanErstellenForm erstellen;
+	private Label kinokettenLabel = new Label ("Kinokette");
+	private Label kinoLabel = new Label ("Kino");
+	private Label spielplanLabel = new Label ("Spielplan");
+	
+	private Grid felder = new Grid (3,2);
 	private HomeBar hb = new HomeBar();
-
+	
+	
 	public void onLoad() {
-		KinoplanerAsync administration = ClientsideSettings.getKinoplaner();
-
+		KinoplanerAsync kinoplaner = ClientsideSettings.getKinoplaner ();
+	
 		this.addStyleName("detailscontainer");
-
+		
 		detailsoben.addStyleName("detailsoben");
+		detailsunten.addStyleName("detailsunten");
 		detailsboxInhalt.addStyleName("detailsboxInhalt");
-		spielplan.addStyleName("title");
-		bearbeitenButton.addStyleName("bearbeitenButton");
+
+		title.addStyleName("title");
+		
 
 		this.add(detailsoben);
+		this.add(detailsunten);
 		this.add(detailsboxInhalt);
-		this.add(bearbeitenButton);
 
-		detailsoben.add(spielplan);
-		detailsoben.add(bearbeitenButton);
-
-		administration.getSpielplaeneByAnwenderOwner(null);
-
-		felder.setWidget(0, 1, spielplan);
-
+		detailsoben.add(hb);
+		detailsoben.add(title);
+		
+		kinokettenLabel.setStyleName("detailsboxLabels");
+		kinoLabel.setStyleName("detailsboxLabels");
+		spielplanLabel.setStyleName("detailsboxLabels");
+		
+		kinoplaner.getSpielplaeneByAnwenderOwner(new SucheSpielplaeneByAnwenderCallback());
+		
+		
 		if (spielplaene != null) {
-			felder.resizeRows(spielplaene.size() + 1);
-			int i = 1;
+			felder.setWidget(0,2, kinokettenLabel);
+			felder.setWidget(0, 1, kinoLabel);
+			felder.setWidget(0, 0,  spielplanLabel);
+			felder.resizeRows(spielplaene.size());
+			int i=1;
+			int j=0;
 			for (Spielplan spielplan : spielplaene) {
-				Label spielplanname = new Label();
+				Label spielplanname = new Label(spielplan.getName());
 				SpielplanAuswaehlenClickHandler click = new SpielplanAuswaehlenClickHandler();
 				click.setSpielplan(spielplan);
 				spielplanname.addDoubleClickHandler(click);
 				felder.setWidget(i, 0, spielplanname);
+				j++;
+				kinoplaner.getKinoById(spielplan.getKinoId(), new KinoByIdCallback());
+				felder.setWidget(i, j, new Label(kino.getName()));
 				i++;
-
+				j=0;
+				kino= null;
+				
 			}
 		} else {
-			felder.setWidget(1, 0, new Label("Keine Spielpläne verfügbar."));
-			Button erstellenButton = new Button("Erstelle deinen ersten Spielplan!");
+			felder.setWidget(0, 0, new Label("Keine Spielpläne verfügbar."));
+			Button erstellenButton = new Button ("Erstelle deinen ersten Spielplan");
 			erstellenButton.setStyleName("navButton");
 			erstellenButton.addDoubleClickHandler(new SpielplanErstellenClickHandler());
 			felder.setWidget(2, 0, erstellenButton);
-
 		}
+		this.add(felder);
 	}
+	
+	private class SpielplanAuswaehlenClickHandler implements DoubleClickHandler {
+		private Spielplan spielplan;
 
+		@Override
+		public void onDoubleClick(DoubleClickEvent event) {
+			RootPanel.get("details").clear();
+			anzeigen = new MeineSpielplaeneForm();
+			anzeigen.setSpielplan(spielplan);
+			RootPanel.get("details").add(anzeigen);
+			
+		}
+		
+	public void setSpielplan (Spielplan spielplan ) {
+		this.spielplan = spielplan ;
+	}
+		
+	}
+	
 	private class SpielplanErstellenClickHandler implements DoubleClickHandler {
 
 		@Override
 		public void onDoubleClick(DoubleClickEvent event) {
-			// TODO Auto-generated method stub
-
-		}
-
-	}
-
-	private class SpielplanAuswaehlenClickHandler implements DoubleClickHandler {
-		private Spielplan spielplan;
-
-		public void onDoubleClick(DoubleClickEvent event) {
 			RootPanel.get("details").clear();
-			anzeigen = new SpielplanAnzeigenForm();
-			RootPanel.get("details").add(anzeigen);
-			anzeigen.setSpielplan(spielplan);
-
+			erstellen = new SpielplanErstellenForm();
+			RootPanel.get("details").add(erstellen);
+			
 		}
-
-		public void setSpielplan(Spielplan spielplan) {
-			this.spielplan = spielplan;
-
-		}
-
+		
+		
 	}
+	
+	private class SucheSpielplanByAnwenderCallback implements AsyncCallback<ArrayList<Spielplan>> {
 
-}
+		@Override
+		public void onFailure(Throwable caught) {
+			Window.alert("Spielplaene nicht abrufbar");
+		}
+
+		@Override
+		public void onSuccess(ArrayList<Spielplan> result) {
+			spielplaene = result;
+		}
+		
+	}
+	
+	private class KinoByIdCallback implements AsyncCallback <Kino> {
+
+		@Override
+		public void onFailure(Throwable caught) {
+			Window.alert("Kino nicht auffindbar");
+			
+		}
+
+		@Override
+		public void onSuccess(Kino result) {
+			kino = result;
+			
+		}
+		
+	}
+} 
+	
+	
