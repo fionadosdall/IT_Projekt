6a5c4388cf9300aa13967163e44d537297d4e327
MoritzHampe <moritzhampe124@web.de>
2019-12-09 16:36:44 +0100

    Update UmfrageErstellenForm.java

--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/editorGui/UmfrageErstellenForm.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/editorGui/UmfrageErstellenForm.java
@@ -2,17 +2,29 @@ package de.hdm.softwareProjekt.kinoPlaner.client.editorGui;
 
 import java.util.ArrayList;
 
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.TextBox;
 
 import de.hdm.softwareProjekt.kinoPlaner.client.ClientsideSettings;
 import de.hdm.softwareProjekt.kinoPlaner.shared.KinoplanerAsync;
+import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Film;
 import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Gruppe;
+import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Kino;
+import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Kinokette;
+import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Spielzeit;
+import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Umfrage;
+import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Umfrageoption;
+import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Vorstellung;
 
 public class UmfrageErstellenForm extends FlowPanel {
 	
@@ -40,6 +52,33 @@ public class UmfrageErstellenForm extends FlowPanel {
 	private Button erstellenButton = new Button("Umfrage starten");
 	
 	private ArrayList<Gruppe> gruppen;
+	
+	private Film film = null;
+	private Spielzeit spielzeit = null;
+	private Kino kino = null;
+	private Kinokette kinokette = null;
+	
+	private ArrayList<Vorstellung> vorstellungen = null;
+	private ArrayList<Umfrageoption> umfrageoptionen = null;
+	 
+	
+	private Grid umfrageGrid = new Grid(2,5);
+	private Grid spielplanGrid = new Grid(2,5);
+	
+	private Label umfrageFilmLabel = new Label("Film");
+	private Label umfrageSpielzeitLabel = new Label("Uhrzeit");
+	private Label umfrageKinoLabel = new Label("Kino");
+	private Label umfrageKinokettenLabel = new Label("Kinokette");
+	private Label umfrageStadtLabel = new Label("Ort");
+
+	private Label filmLabel = new Label("Film");
+	private Label spielzeitLabel = new Label("Uhrzeit");
+	private Label kinoLabel = new Label("Kino");
+	private Label kinokettenLabel = new Label("Kinokette");
+	private Label stadtLabel = new Label("Ort");
+	
+	private ScrollPanel scrollPanelUmfrage = new ScrollPanel();
+	private ScrollPanel scrollPanelSpielplan = new ScrollPanel();
 
 	
 	public void onLoad() {
@@ -70,6 +109,22 @@ public class UmfrageErstellenForm extends FlowPanel {
 		umfrageTextBox.addStyleName("gruppenameTB");
 		
 		umfrageTextBox.getElement().setPropertyString("placeholder", "Anlass für die Umfrage?");
+
+		
+		umfrageGrid.setWidget(0, 0, umfrageFilmLabel);
+		umfrageGrid.setWidget(0, 1, umfrageSpielzeitLabel);
+		umfrageGrid.setWidget(0, 2, umfrageKinoLabel);
+		umfrageGrid.setWidget(0, 3, umfrageKinokettenLabel);
+		umfrageGrid.setWidget(0, 4, umfrageStadtLabel);
+		
+		spielplanGrid.setWidget(0, 0, filmLabel);
+		spielplanGrid.setWidget(0, 1, spielzeitLabel);
+		spielplanGrid.setWidget(0, 2, kinoLabel);
+		spielplanGrid.setWidget(0, 3, kinokettenLabel);
+		spielplanGrid.setWidget(0, 4, stadtLabel);
+		
+		scrollPanelUmfrage.setHeight("30px");
+		scrollPanelSpielplan.setHeight("30px");
 		
 		// Zusammenbauen der Widgets
 		
@@ -90,12 +145,19 @@ public class UmfrageErstellenForm extends FlowPanel {
 		
 		detailsunten.add(detailsBoxUmfrage);
 		detailsBoxUmfrage.add(terminLabel);
+		detailsBoxUmfrage.add(scrollPanelUmfrage);
+		detailsBoxUmfrage.add(scrollPanelSpielplan);
+		scrollPanelUmfrage.add(umfrageGrid);
+		scrollPanelSpielplan.add(spielplanGrid);	
 		
 		detailsunten.add(detailsBoxFiltern);
 		detailsBoxFiltern.add(filternLabel);
 		
 		detailsunten.add(detailsBoxSpeichern);
 		detailsBoxSpeichern.add(erstellenButton);
+		
+		//KlickHandler
+		erstellenButton.addClickHandler(new UmfrageErstellenClickHandler());
 		
 		gruppenListBox.setSize("200px", "25px");
 		
@@ -110,6 +172,23 @@ public class UmfrageErstellenForm extends FlowPanel {
 			for (Gruppe g : gruppen) {
 				gruppenListBox.addItem(g.getName());
 			}
+		}
+		
+		
+		
+		kinoplaner.getAllVorstellungen(new VorstellungenCallback());
+
+		
+	}
+	
+	
+	private class UmfrageErstellenClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+		//	kinoplaner.erstellenUmfrage(umfrageTextBox.getValue(), gruppenListBox, new UmfrageErstellenCallback());
+			
 		}
 		
 	}
@@ -130,5 +209,33 @@ public class UmfrageErstellenForm extends FlowPanel {
 		
 	}
 	
+	private class VorstellungenCallback implements AsyncCallback<ArrayList<Vorstellung>> {
+
+		@Override
+		public void onFailure(Throwable caught) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onSuccess(ArrayList<Vorstellung> result) {
+			
+		}
+}
+	private class UmfrageErstellenCallback implements AsyncCallback<Umfrage> {
+
+		@Override
+		public void onFailure(Throwable caught) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onSuccess(Umfrage result) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 
 }
