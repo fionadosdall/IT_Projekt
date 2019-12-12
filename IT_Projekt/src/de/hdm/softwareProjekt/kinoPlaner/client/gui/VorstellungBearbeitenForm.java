87f97b1b86bd30fc0a0a6f6f137f7763e5ce2916
fionadosdall <56394639+fionadosdall@users.noreply.github.com>
2019-12-12 13:49:55 +0100

    VorstellungBearbeitenForm - Klasse ersteltt

--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/gui/VorstellungBearbeitenForm.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/gui/VorstellungBearbeitenForm.java
@@ -1,5 +1,26 @@
 package de.hdm.softwareProjekt.kinoPlaner.client.gui;
 
-public class VorstellungBearbeitenForm {
+import com.google.gwt.user.client.ui.FlowPanel;
+
+import de.hdm.softwareProjekt.kinoPlaner.client.ClientsideSettings;
+import de.hdm.softwareProjekt.kinoPlaner.shared.KinoplanerAsync;
+
+public class VorstellungBearbeitenForm extends FlowPanel {
+	
+	KinoplanerAsync kinoplaner = ClientsideSettings.getKinoplaner();
+	
+	private FlowPanel detailsoben = new FlowPanel();
+	private FlowPanel detailsunten = new FlowPanel();
+	private FlowPanel speichernBox = new FlowPanel();
+	private FlowPanel detailsBoxObenMitte = new FlowPanel();
+	private FlowPanel detailsBoxMitteMitte = new FlowPanel();
+	private FlowPanel detailsBoxMitteUnten = new FlowPanel();
+	private FlowPanel detailsBoxUntenMitte = new FlowPanel();
+	private FlowPanel detailsBoxUnten = new FlowPanel();
+	private FlowPanel detailsObenBox = new FlowPanel();
+	private FlowPanel detailsMitteBox = new FlowPanel();
+	private FlowPanel detailsUntenBox = new FlowPanel();
+	
+	
 
 }
