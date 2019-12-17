764a5abeb28fcbeb7af883485568deac011b7128
fionadosdall <56394639+fionadosdall@users.noreply.github.com>
2019-12-17 11:31:06 +0100

    SpielplaneintragErstellen bearbeitet

--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/gui/SpielplaneintragErstellen.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/gui/SpielplaneintragErstellen.java
@@ -1,6 +1,7 @@
 package de.hdm.softwareProjekt.kinoPlaner.client.gui;
 
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -9,18 +10,35 @@ import de.hdm.softwareProjekt.kinoPlaner.client.ClientsideSettings;
 import de.hdm.softwareProjekt.kinoPlaner.shared.KinoplanerAsync;
 
 
-public class SpielplaneintragErstellen extends VerticalPanel {
+public class SpielplaneintragErstellen extends FlowPanel {
 	
-	private HorizontalPanel obenPanel = new HorizontalPanel();
-	private HorizontalPanel untenPanel = new HorizontalPanel();
+	
 
-	private KinoplanerAsync administration = ClientsideSettings.getKinoplaner();
+	private KinoplanerAsync kinoplaner = ClientsideSettings.getKinoplaner();
 	
-	private Label spielplaneintragFormLabel = new Label("Neuer Spielplaneintrag");
-	private Label nameLabel = new Label ("Name:");
+	private FlowPanel detailsoben = new FlowPanel();
+	private FlowPanel detailsunten = new FlowPanel();
+	private FlowPanel speichernBox = new FlowPanel();
+	private FlowPanel detailsBoxObenMitte = new FlowPanel();
+	private FlowPanel detailsBoxMitteMitte = new FlowPanel();
+	private FlowPanel detailsBoxMitteUnten = new FlowPanel();
+	private FlowPanel detailsBoxUntenMitte = new FlowPanel();
+	private FlowPanel detailsBoxUnten = new FlowPanel();
+	private FlowPanel detailsObenBox = new FlowPanel();
+	private FlowPanel detailsMitteBox = new FlowPanel();
+	private FlowPanel detailsUntenBox = new FlowPanel();
+	
+	
+	
+	private Label title = new Label("Neuer Spielplaneintrag");
+	private Label spielplaneintragname = new Label ("Name:");
 	private Label filmLabel = new Label ("Film:");
 	private Label spielzeitLabel = new Label("Spielzeit:");
 	
+	
+	
+	private Button hinzufuegenButton = new Button("Hinzufügen");
+	private Button entfernenButton = new Button ("entfernen");
 	private Button speichernButton = new Button("Speichern");
 	
 	
