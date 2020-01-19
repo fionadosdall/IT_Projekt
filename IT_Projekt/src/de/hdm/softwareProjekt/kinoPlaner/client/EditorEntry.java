52a85061983454cb40fec7daede59ee5b8f8e876
annaFessmann <af093@hdm-stuttgart.de>
2020-01-19 11:04:06 +0100

    EditorEntry kommentiert, Test

--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/EditorEntry.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/EditorEntry.java
@@ -1,6 +1,5 @@
 package de.hdm.softwareProjekt.kinoPlaner.client;
 
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -14,72 +13,87 @@ import de.hdm.softwareProjekt.kinoPlaner.shared.KinoplanerAsync;
 import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Anwender;
 
 /**
- * Entry point classes define <code>onModuleLoad()</code>.
+ * EntryPoint-Klasse des Editors. Entry point classes define
+ * <code>onModuleLoad()</code>. Denn diese Klasse implementiert das Interface
+ * EntryPoint, demzufolge muss auch die Methode onModuleLoad() definiert sein.
+ * Die Methode wird beim Seitenaufruf zu Beginn aufgerufen.
+ * 
  */
 public class EditorEntry implements EntryPoint {
-	
+
 	Header header = new Header();
 	Footer footer = new Footer();
 	private KinoplanerAsync kinoplaner = ClientsideSettings.getKinoplaner();
-	
-	
 
-	
 	public void onModuleLoad() {
 		kinoplaner.getAnwenderById(1, new GetAnwenderByIdCallback());
-	
-		
-		
-		
-		
+
 	}
+
+	/*
+	 * Im Folgenden erstellt die onModuleLoad()-Methode Instanzen des asynchronen
+	 * Interfaces KinoplanerAsync:
+	 */
+
 	private class GetAnwenderByIdCallback implements AsyncCallback<Anwender> {
 
+		/*
+		 * Wenn fehlgeschlagen: Ein neuer User soll erstellt werden --> new
+		 * AnwenderErstellenCallback()
+		 */
 		@Override
 		public void onFailure(Throwable caught) {
 			kinoplaner.erstellenAnwender("Hansi Test", "testmail@test.de", new AnwenderErstellenCallback());
-			
+
 		}
 
+		/*
+		 * Wenn ein User eingeloggt ist, wird dieser zurückgegeben. Andernfalls soll ein
+		 * neuer User erstellt werden --> new AnwenderErstellenCallback()
+		 */
 		@Override
 		public void onSuccess(Anwender result) {
 			if (result.getId() == 1) {
 				kinoplaner.setAnwender(result, new SetAnwenderCallback());
 				aktuellerAnwender.setAnwender(result);
-			}else {
+			} else {
 				kinoplaner.erstellenAnwender("Hansi Test", "testmail@test.de", new AnwenderErstellenCallback());
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	private class AnwenderErstellenCallback implements AsyncCallback<Anwender> {
 
 		@Override
 		public void onFailure(Throwable caught) {
 			Window.alert("Initilanwender konnte nicht erstellt werden");
 			Window.alert(caught.getMessage());
-			
+
 		}
 
 		@Override
 		public void onSuccess(Anwender result) {
 			kinoplaner.setAnwender(result, new SetAnwenderCallback());
 			aktuellerAnwender.setAnwender(result);
-			
+
 		}
-		
+
 	}
-	
+
 	private class SetAnwenderCallback implements AsyncCallback<Void> {
 
 		@Override
 		public void onFailure(Throwable caught) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
+		/*
+		 * Bei Erfolg: Der User in der Rolle als Editor eingeloggt und erhält als
+		 * Startseite automatisch eine Übersicht seiner Umfragen.
+		 */
 		@Override
 		public void onSuccess(Void result) {
 			RootPanel.get("header").add(header);
@@ -87,24 +101,26 @@ public class EditorEntry implements EntryPoint {
 			UmfragenAnzeigenForm uaf = new UmfragenAnzeigenForm();
 			RootPanel.get("details").add(uaf);
 		}
-		
+
 	}
-	
+
 	/*
-	 * Die Klasse dient jederzeit zum Aufrufen des aktuellen Anwenders
+	 * Diese Klasse repräsentiert den User, der aktuell im Kinoplaner eingeloggt
+	 * ist. Die Klasse ist public, damit global auf sie zugegriffen werden kann
+	 * (z.B. von anderen GUI Klassen)
 	 */
-	
+
 	public static class aktuellerAnwender {
-		
+
 		public static Anwender anwender = null;
-		
+
 		public static Anwender getAnwender() {
 			return anwender;
 		}
-		
+
 		public static void setAnwender(Anwender anwender) {
 			aktuellerAnwender.anwender = anwender;
 		}
-		
+
 	}
 }
