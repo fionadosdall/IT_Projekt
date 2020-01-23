9f081008ec1629ab8cdde4dbc952a78242ca5448
annaFessmann <af093@hdm-stuttgart.de>
2020-01-23 16:09:19 +0100

    Update VolltextsucheForm

--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/editorGui/VolltextSucheForm.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/editorGui/VolltextSucheForm.java
@@ -18,6 +18,14 @@ import de.hdm.softwareProjekt.kinoPlaner.shared.KinoplanerAsync;
 import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Gruppe;
 import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Umfrage;
 
+/**
+ * Klasse zur Volltextsuche: Auf allen Seiten des Kinoplaners steht dem Nutzer
+ * im Header eine Volltextsuche zur Verfügung. Gesucht werden können Umfragen,
+ * Gruppen und Ergebnisse.
+ * 
+ * @author
+ *
+ */
 public class VolltextSucheForm extends FlowPanel {
 	private String suchText;
 	private ArrayList<Gruppe> gruppen;
@@ -46,8 +54,12 @@ public class VolltextSucheForm extends FlowPanel {
 	private Grid gruppenGrid = new Grid(3, 2);
 	private Grid umfragenGrid = new Grid(3, 2);
 	private Grid ergebnisseGrid = new Grid(3, 2);
-	
-	
+
+	/**
+	 * Konstruktor
+	 * 
+	 * @param text
+	 */
 
 	public VolltextSucheForm(String text) {
 		this.suchText = text;
@@ -55,7 +67,11 @@ public class VolltextSucheForm extends FlowPanel {
 
 	@Override
 	protected void onLoad() {
+
 		super.onLoad();
+
+		// Stylenamen vergeben
+
 		detailsoben.addStyleName("detailsoben");
 		detailsunten.addStyleName("detailsunten");
 
@@ -74,6 +90,8 @@ public class VolltextSucheForm extends FlowPanel {
 		gruppe.addStyleName("detailsboxLabels");
 		umfrage.addStyleName("detailsboxLabels");
 		ergebnis.addStyleName("detailsboxLabels");
+
+		// Widgets zusammen bauen
 
 		this.add(detailsoben);
 		this.add(detailsunten);
@@ -96,16 +114,31 @@ public class VolltextSucheForm extends FlowPanel {
 		detailsUntenBox.add(detailsBoxUntenMitte);
 		detailsBoxUntenMitte.add(ergebnisseGrid);
 
+		/**
+		 * Hinzufügen der Suche-Callbacks von Gruppe, Umfrage und Ergebnis
+		 */
+
 		kinoplaner.volltextSucheGruppen(suchText, new VolltextSucheGruppenCallback());
-		
 
 		kinoplaner.volltextSucheUmfragen(suchText, new VolltextSucheUmfrageCallback());
-		
 
 		kinoplaner.volltextSucheErgebnisse(suchText, new VolltextSucheErgebnisseCallback());
-		
 
 	}
+
+	/******************************
+	 * CLICKHANDLER
+	 * 
+	 *
+	 *******************************/
+
+	/**
+	 * ClickHandler, um bei der Volltextsuche gefundene Ergebnisse auswählen zu
+	 * können. Durch Klicken auf das Ergebnis wird der Nutzer automatisch zur
+	 * ErgebnisAnzeigenForm weitergeleitet.
+	 * 
+	 *
+	 */
 
 	private class ErgebnisAuswaehlenClickHandler implements DoubleClickHandler {
 
@@ -126,6 +159,13 @@ public class VolltextSucheForm extends FlowPanel {
 
 	}
 
+	/**
+	 * Neue Umfragen können erstellt werden, durch Klicken gelangt der Nutzer
+	 * automatisch auf das Formular, um Umfragen erstellen zu können
+	 * (UmfrageErstellenForm).
+	 * 
+	 *
+	 */
 	private class UmfrageErstellenClickHandler implements ClickHandler {
 
 		@Override
@@ -137,6 +177,12 @@ public class VolltextSucheForm extends FlowPanel {
 		}
 
 	}
+
+	/**
+	 * ClickHandler, um bei der Volltextsuche gefundene Umfragen auswählen zu
+	 * können. Durch Klicken auf die Umfrage, wird der Nutzer automatisch zur
+	 * UmfrageAnzeigenForm weitergeleitet.
+	 */
 
 	private class UmfrageAuswaehlenClickHandler implements DoubleClickHandler {
 		private Umfrage umfrage;
@@ -156,6 +202,14 @@ public class VolltextSucheForm extends FlowPanel {
 
 	}
 
+	/**
+	 * Eine neue Gruppe kann erstellt werden. Durch Klicken wird der Nutzer
+	 * automatisch zum Formular weitergeleitet, in dem er eine Gruppe anlegen kann
+	 * (GruppeErstellenForm)
+	 * 
+	 * @author
+	 *
+	 */
 	private class GruppeErstellenClickHandler implements ClickHandler {
 
 		@Override
@@ -167,6 +221,12 @@ public class VolltextSucheForm extends FlowPanel {
 		}
 
 	}
+
+	/**
+	 * ClickHandler, um bei der Volltextsuche gefundene Gruppen auswählen zu können.
+	 * Durch Klicken auf die Gruppe, wird der Nutzer automatisch zur
+	 * GruppeAnzeigenForm weitergeleitet.
+	 */
 
 	private class GruppeAuswaehlenClickHandler implements DoubleClickHandler {
 
@@ -186,6 +246,18 @@ public class VolltextSucheForm extends FlowPanel {
 
 	}
 
+	/*********************************
+	 * CALLBACK
+	 *
+	 *********************************/
+
+	/**
+	 * Callback für Umfragenergebnisse, die durch die Volltextsuche gefunden wurden. Result
+	 * der Suche = alle gefundenen Umfragenergebnisse in einer ArrayList. Ergebnisse werden in
+	 * einem Grid dargestellt. Die Einträge im Grid reagieren auf Doppelklicks. Per
+	 * Doppelklick gelangt der Nutzer in die Detailsansicht des jeweiligen Elements
+	 * (hier das Umfrageergebnis).
+	 */
 	private class VolltextSucheErgebnisseCallback implements AsyncCallback<ArrayList<Umfrage>> {
 
 		@Override
@@ -197,9 +269,9 @@ public class VolltextSucheForm extends FlowPanel {
 		@Override
 		public void onSuccess(ArrayList<Umfrage> result) {
 			ergebnisse = result;
-			
-			if (ergebnisse.size() !=0) {
-				ergebnisseGrid.resizeRows(ergebnisse.size()+2);
+
+			if (ergebnisse.size() != 0) {
+				ergebnisseGrid.resizeRows(ergebnisse.size() + 2);
 				int i = 0;
 				for (Umfrage e : ergebnisse) {
 					Label ergebnissename = new Label(e.getName());
@@ -209,7 +281,7 @@ public class VolltextSucheForm extends FlowPanel {
 					ergebnisseGrid.setWidget(i, 0, ergebnissename);
 					i++;
 				}
-				ergebnisseGrid.setWidget(i+1, 0, new Label("Doppelklicken um das Ergebniss anzuzeigen"));
+				ergebnisseGrid.setWidget(i + 1, 0, new Label("Doppelklicken um das Ergebniss anzuzeigen"));
 			} else {
 				ergebnisseGrid.setWidget(0, 0, new Label("Keine Ergebnisse gefunden."));
 
@@ -219,6 +291,14 @@ public class VolltextSucheForm extends FlowPanel {
 
 	}
 
+	/**
+	 * Callback für Umfragen, die durch die Volltextsuche gefunden wurden. Result
+	 * der Suche = alle gefundenen Umfragen in einer ArrayList. Ergebnisse werden in
+	 * einem Grid dargestellt. Die Einträge im Grid reagieren auf Doppelklicks. Per
+	 * Doppelklick gelangt der Nutzer in die Detailsansicht des jeweiligen Elements
+	 * (hier die Umfrage).
+	 */
+	
 	private class VolltextSucheUmfrageCallback implements AsyncCallback<ArrayList<Umfrage>> {
 
 		@Override
@@ -230,9 +310,9 @@ public class VolltextSucheForm extends FlowPanel {
 		@Override
 		public void onSuccess(ArrayList<Umfrage> result) {
 			umfragen = result;
-			
-			if (umfragen.size() !=0) {
-				umfragenGrid.resizeRows(umfragen.size()+2);
+
+			if (umfragen.size() != 0) {
+				umfragenGrid.resizeRows(umfragen.size() + 2);
 				int i = 0;
 				for (Umfrage u : umfragen) {
 					Label umfragename = new Label(u.getName());
@@ -242,7 +322,7 @@ public class VolltextSucheForm extends FlowPanel {
 					umfragenGrid.setWidget(i, 0, umfragename);
 					i++;
 				}
-				umfragenGrid.setWidget(i+1, 0, new Label("Doppelklicken um die Umfrage anzuzeigen"));
+				umfragenGrid.setWidget(i + 1, 0, new Label("Doppelklicken um die Umfrage anzuzeigen"));
 			} else {
 				umfragenGrid.setWidget(0, 0, new Label("Keine Umfragen gefunden."));
 				Button erstellenButton = new Button("Erstelle eine Umfrage!");
@@ -256,6 +336,13 @@ public class VolltextSucheForm extends FlowPanel {
 
 	}
 
+	/**
+	 * Callback für Gruppen, die durch die Volltextsuche gefunden wurden. Result
+	 * der Suche = alle gefundenen Gruppen in einer ArrayList. Ergebnisse werden in
+	 * einem Grid dargestellt. Die Einträge im Grid reagieren auf Doppelklicks. Per
+	 * Doppelklick gelangt der Nutzer in die Detailsansicht des jeweiligen Elements
+	 * (hier die Gruppe).
+	 */
 	private class VolltextSucheGruppenCallback implements AsyncCallback<ArrayList<Gruppe>> {
 
 		@Override
@@ -267,9 +354,9 @@ public class VolltextSucheForm extends FlowPanel {
 		@Override
 		public void onSuccess(ArrayList<Gruppe> result) {
 			gruppen = result;
-			
-			if (gruppen.size() !=0) {
-				gruppenGrid.resizeRows(gruppen.size()+2);
+
+			if (gruppen.size() != 0) {
+				gruppenGrid.resizeRows(gruppen.size() + 2);
 				int i = 0;
 				for (Gruppe g : gruppen) {
 					Label gruppenename = new Label(g.getName());
@@ -279,7 +366,7 @@ public class VolltextSucheForm extends FlowPanel {
 					gruppenGrid.setWidget(i, 0, gruppenename);
 					i++;
 				}
-				gruppenGrid.setWidget(i+1, 0, new Label("Doppelklicken um die Gruppe anzuzeigen"));
+				gruppenGrid.setWidget(i + 1, 0, new Label("Doppelklicken um die Gruppe anzuzeigen"));
 			} else {
 				gruppenGrid.setWidget(0, 0, new Label("Keine Gruppen gefunden."));
 				Button erstellenButton = new Button("Erstelle eine Gruppe!");
