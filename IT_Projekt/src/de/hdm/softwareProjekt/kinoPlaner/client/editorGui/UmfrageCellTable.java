c790c8ae3e04a5e7f97c0550bf9b9d5833c95c20
annaFessmann <af093@hdm-stuttgart.de>
2020-01-25 20:51:56 +0100

    UmfrageCellTable fix und comments

--- IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/editorGui/UmfrageCellTable.java
+++ IT_Projekt/src/de/hdm/softwareProjekt/kinoPlaner/client/editorGui/UmfrageCellTable.java
@@ -28,7 +28,9 @@ import de.hdm.softwareProjekt.kinoPlaner.shared.bo.Vorstellung;
 public class UmfrageCellTable extends ScrollPanel {
 
 	public interface CellTableResources extends CellTable.Resources {
-
+		/**
+		 * Styling der CellTable erfolgt über das CellTable.css Stylesheet
+		 */
 		@Source({ CellTable.Style.DEFAULT_CSS, "CellTable.css" })
 		TableStyle cellTableStyle();
 
@@ -40,6 +42,15 @@ public class UmfrageCellTable extends ScrollPanel {
 
 	KinoplanerAsync kinoplaner = ClientsideSettings.getKinoplaner();
 
+	/**
+	 * Das zugrundeliegende Objekt der UmfrageCelltables sind Vorstellungen. Jedoch
+	 * speichert das gewöhnliche BusinessObject Vorstellung seine Attribute nicht in
+	 * Klarnamen. Aufgrund dessen erstellen wir hier für die CellTable das Objekt
+	 * VorstellungInfo. Dort werden die Attribute filmName, kinoName, spielzeit und
+	 * stadt in Klarnamen gespeichert. Inklusive Getter und Setter.
+	 * 
+	 *
+	 */
 	private class VorstellungInfo {
 
 		Vorstellung u;
@@ -89,12 +100,22 @@ public class UmfrageCellTable extends ScrollPanel {
 
 	private CellTable<VorstellungInfo> umfrageCellTable = new CellTable<VorstellungInfo>(100, tableRes);
 
+	/**
+	 * ListDataProvider kümmert sich darum, dass die Daten dargestellt werden und wo
+	 * sie dargestellt werden (in der CellTable). Später kann dem ListDataProvider
+	 * den kompletten Datensatz übergeben und er kümmert sich automatisch darum.
+	 * 
+	 */
 	private ListDataProvider<VorstellungInfo> dataProviderUmfrage = new ListDataProvider<VorstellungInfo>();
 
 	public ListDataProvider<VorstellungInfo> getDataProviderUmfrage() {
 		return dataProviderUmfrage;
 	}
 
+	/**
+	 * Der ListDataProvider hat eine Liste im Hintergrund, in der er zunächst alles
+	 * speichert.
+	 */
 	private List<VorstellungInfo> umfrageList = dataProviderUmfrage.getList();
 
 	public List<VorstellungInfo> getUmfrageList() {
@@ -104,8 +125,6 @@ public class UmfrageCellTable extends ScrollPanel {
 	public void setUmfrageList(List<VorstellungInfo> umfrageList) {
 		this.umfrageList = umfrageList;
 	}
-
-	
 
 	private ButtonCell buttonCell = new ButtonCell();
 	private TextCell filmCell = new TextCell();
@@ -130,18 +149,31 @@ public class UmfrageCellTable extends ScrollPanel {
 		return vorstellung;
 	}
 
+	/**
+	 * onLoad()-Methode
+	 */
 	public void onLoad() {
-		
+
 		this.setSize("100%", "250px");
 
 		this.add(umfrageCellTable);
 		umfrageCellTable.setWidth("100%");
-		
+
 		ListHandler<VorstellungInfo> sortHandler = new ListHandler<VorstellungInfo>(umfrageList);
 		umfrageCellTable.addColumnSortHandler(sortHandler);
 
+		/**
+		 * Eine neue Spalte soll erstellt werden. Sie beinhaltet zwei Datentypen:
+		 * VorstellungInfo ist das der CellTable zugrundeliegende Objekt, String ist der
+		 * Datentyp, welcher in dieser Spalte dargestellt wird.
+		 */
 		Column<VorstellungInfo, String> buttonColumn = new Column<VorstellungInfo, String>(buttonCell) {
 
+			/**
+			 * Wenn man eine neue Spalte erstellt, muss man die Methode getValue()
+			 * überschreiben (wegen Annotation Override). In der Methode wird festgelegt,
+			 * was der Inhalt der Spalte ist/ bzw. returnt.
+			 */
 			@Override
 			public String getValue(VorstellungInfo object) {
 				// TODO Auto-generated method stub
@@ -150,25 +182,42 @@ public class UmfrageCellTable extends ScrollPanel {
 
 		};
 
+		/**
+		 * Nun muss die neu erstellte Spalte der UmfrageCellTable hinzugefügt werden.
+		 * Unter Angabe der Bezeichnung der Spalte und ihrer Überschrift. Hinweis: In
+		 * der Reihenfolge, in der die Spalten der CellTable hinzugefügt werden, werden
+		 * sie auch später für den Nutzer angezeigt.
+		 */
 		umfrageCellTable.addColumn(buttonColumn, "Entfernen");
-		
-		
 
+		/**
+		 * Spalten kann ein FieldUpdater hinzugefügt werden. FieldUpdater schaut, ob
+		 * sich etwas in der Spalte verändert hat und updatet entsprechend. Dazu müssen
+		 * dem FieldUpdater die selben Datentypen zugewiesen werden, wie der Spalte
+		 * selbst.
+		 */
 		buttonColumn.setFieldUpdater(new FieldUpdater<VorstellungInfo, String>() {
 
+			/**
+			 * FieldUpdater (Interface) fordert, dass Methode update() überschrieben wird
+			 * (Annotation Override). Methode update() beschreibt was gemacht werden soll, wenn der Nutzer
+			 * auf den CellButton drückt. Hier: Es handelt sich um Entfernen-Button, also .remove(object)
+			 * 
+			 */
 			@Override
 			public void update(int index, VorstellungInfo object, String value) {
 				// TODO Auto-generated method stub
-				
-				dataProviderUmfrage.getList().remove(object); 
+
+				dataProviderUmfrage.getList().remove(object);
 				dataProviderUmfrage.refresh();
 				nct.addVorstellung(object.getU());
-				
 
 			}
 		});
-		
 
+		/**
+		 * Erstellen der zweiten Column:
+		 */
 		Column<VorstellungInfo, String> filmColumn = new Column<VorstellungInfo, String>(filmCell) {
 
 			@Override
@@ -179,7 +228,11 @@ public class UmfrageCellTable extends ScrollPanel {
 
 			}
 		};
-		
+
+		/**
+		 * Spalte soll die Möglichkeit zu Sortieren bieten. Spalten werden durch einen
+		 * Vergleichsmechanismus sortiert.
+		 */
 		filmColumn.setSortable(true);
 
 		sortHandler.setComparator(filmColumn, new Comparator<VorstellungInfo>() {
@@ -190,6 +243,9 @@ public class UmfrageCellTable extends ScrollPanel {
 
 		umfrageCellTable.addColumn(filmColumn, "Film");
 
+		/**
+		 * Erstellen der dritten Column:
+		 */
 		Column<VorstellungInfo, String> kinoColumn = new Column<VorstellungInfo, String>(kinoCell) {
 
 			@Override
@@ -200,7 +256,7 @@ public class UmfrageCellTable extends ScrollPanel {
 
 			}
 		};
-		
+
 		kinoColumn.setSortable(true);
 
 		sortHandler.setComparator(kinoColumn, new Comparator<VorstellungInfo>() {
@@ -211,6 +267,9 @@ public class UmfrageCellTable extends ScrollPanel {
 
 		umfrageCellTable.addColumn(kinoColumn, "Kino");
 
+		/**
+		 * Erstellen der vierten Column
+		 */
 		Column<VorstellungInfo, String> speilzeitColumn = new Column<VorstellungInfo, String>(spielzeitCell) {
 
 			@Override
@@ -223,7 +282,7 @@ public class UmfrageCellTable extends ScrollPanel {
 		};
 
 		umfrageCellTable.addColumn(speilzeitColumn, "Spielzeit");
-		
+
 		speilzeitColumn.setSortable(true);
 
 		sortHandler.setComparator(speilzeitColumn, new Comparator<VorstellungInfo>() {
@@ -232,8 +291,9 @@ public class UmfrageCellTable extends ScrollPanel {
 			}
 		});
 
-		
-
+		/**
+		 * Erstellen der fünften Column
+		 */
 		Column<VorstellungInfo, String> stadtColumn = new Column<VorstellungInfo, String>(stadtCell) {
 
 			@Override
@@ -246,7 +306,7 @@ public class UmfrageCellTable extends ScrollPanel {
 		};
 
 		umfrageCellTable.addColumn(stadtColumn, "Ort");
-		
+
 		stadtColumn.setSortable(true);
 
 		sortHandler.setComparator(stadtColumn, new Comparator<VorstellungInfo>() {
@@ -255,30 +315,41 @@ public class UmfrageCellTable extends ScrollPanel {
 			}
 		});
 
+		/**
+		 * Die Methode addDataDisplay() legt über ihren Übergabeparameter für den
+		 * DataProvider fest, wo er nun seine Informationen bzw. den Umfrage-Datensatz
+		 * verwalten soll.
+		 */
 		dataProviderUmfrage.addDataDisplay(umfrageCellTable);
 
 		this.addUmfrageoption(neueUmfrageoptionen);
 
-
 	}
 
+	/**
+	 * Alle add/hinzufügen-Vorgänge, um der CellTable etwas hinzuzufügen, erfolgen
+	 * über die Liste, die im Hintergrund des DataProviders arbeitet. (Hier:
+	 * umfrageList)
+	 * 
+	 * @param neueUmfrageoptionen
+	 */
 	public void addUmfrageoption(ArrayList<Vorstellung> neueUmfrageoptionen) {
 
 		this.neueUmfrageoptionen = nct.getUmfrageOptionen();
 
 		if (this.neueUmfrageoptionen.size() != 0) {
-			
+
 			umfrageList.clear();
 
 			for (Vorstellung v : this.neueUmfrageoptionen) {
 
-				//Window.alert(v.getName());
+				// Window.alert(v.getName());
 
 				uI = new VorstellungInfo();
 
 				uI.setU(v);
 
-				//Window.alert(v.getName());
+				// Window.alert(v.getName());
 
 				umfrageList.add(uI);
 
@@ -291,7 +362,6 @@ public class UmfrageCellTable extends ScrollPanel {
 		}
 
 	}
-
 
 	private class FilmByIdCallback implements AsyncCallback<Film> {
 
@@ -311,8 +381,16 @@ public class UmfrageCellTable extends ScrollPanel {
 		public void onSuccess(Film result) {
 			// TODO Auto-generated method stub
 
+			/**
+			 * Die Informationen, die man über den Film erhalten hat, werden dem
+			 * zugrundeliegenden Objekt VorstellungInfo hinzugefügt.
+			 */
 			info.filmName = result.getName();
 
+			/**
+			 * DataProvider liefert die Methode refresh(), dadruch kann der Anzeigeinhalt
+			 * bei erfolgreichem Callback, hier: um den Film, aktualisiert werden.
+			 */
 			dataProviderUmfrage.refresh();
 
 		}
@@ -337,10 +415,18 @@ public class UmfrageCellTable extends ScrollPanel {
 		public void onSuccess(Kino result) {
 			// TODO Auto-generated method stub
 
+			/**
+			 * Die Informationen, die man über das Kino erhalten hat, werden dem
+			 * zugrundeliegenden Objekt VorstellungInfo hinzugefügt.
+			 */
 			info.kinoName = result.getName();
 
 			info.stadt = result.getStadt();
 
+			/**
+			 * Über die refresh()-Methode des DataProviders kann der Anzeigeninhalt
+			 * entsprechend der Informationen zum Kino aktualisiert werden.
+			 */
 			dataProviderUmfrage.refresh();
 
 		}
@@ -363,13 +449,21 @@ public class UmfrageCellTable extends ScrollPanel {
 
 		@Override
 		public void onSuccess(Spielzeit result) {
+			/**
+			 * Spielzeit formatieren
+			 */
 			DefaultDateTimeFormatInfo infoDDTFI = new DefaultDateTimeFormatInfo();
 			String pattern = "EEEE dd.MM.yyyy HH:mm";
 			DateTimeFormat dft = new DateTimeFormat(pattern, infoDDTFI) {
 			};
 
+			/**
+			 * Spielzeit zum VorstellungInfo-Objekt
+			 */
 			info.spielzeit = dft.format(result.getZeit());
-
+			/**
+			 * Anzeigeninhalt bezüglich Spielzeit aktualisieren.
+			 */
 			dataProviderUmfrage.refresh();
 
 		}
